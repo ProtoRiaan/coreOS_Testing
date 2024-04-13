016af4ad50a0d38a2495aaaf18dfe881abe311da
Riaan Schuld <rhschuld@gmail.com>
2024-04-13 19:50:20 -0400

    did some cleanup. Testing local ssh key oub files

--- butane.sh
+++ butane.sh
@@ -1,4 +1,5 @@
 #!bin/bash
 docker run --interactive --rm --security-opt label=disable\
        --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --files-dir /pwd \
        --pretty --strict init.yaml > init.ign
