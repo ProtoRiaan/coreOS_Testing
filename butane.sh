b26416432977ce383126726898d58a3c2d9111e9
Riaan Schuld <rhschuld@gmail.com>
2024-04-13 18:47:20 -0400

    more security updates and added bash script to run butane

---
+++ butane.sh
@@ -0,0 +1,4 @@
+#!bin/bash
+docker run --interactive --rm --security-opt label=disable\
+       --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
+       --pretty --strict init.yaml > init.ign
