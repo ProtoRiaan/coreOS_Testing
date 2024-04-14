84554f27c295cd852afe4ec4a9518e9d05a471bc
Riaan Schuld <rhschuld@gmail.com>
2024-04-13 20:17:41 -0400

    fixed the butane script

--- butane.sh
+++ butane.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 docker run --interactive --rm --security-opt label=disable\
        --volume ${PWD}:/pwd --workdir /pwd quay.io/coreos/butane:release \
        --files-dir /pwd \
